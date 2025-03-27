4756cd6e210103fbf338a5723073c14b385fd3e0
shusin2798 <shubham.singh@syncron.com>
2025-03-27 10:34:40 +0530

    Implement helpers.go to enforce rules on the domain:3

---
+++ go-url-shortner/api/helpers/helpers.go
@@ -0,0 +1,28 @@
+package helpers
+
+import (
+	"os"
+	"strings"
+)
+
+func EnforceHTTP() string {
+	if url[:4] != "http" {
+		return "http://" + url
+	}
+	return url
+}
+
+func RemoveDomainError(url string) bool {
+	if url == os.Getenv("DOMAIN") {
+		return false
+	}
+	newURL := strings.Replace(url, "www.", "", 1)
+	newURL = strings.Replace(newURL, "http://", "", 1)
+	newURL = strings.Replace(newURL, "https://", "", 1)
+	newURL = strings.Replace(newURL, "/", "", 1)
+
+	if newURL == os.Getenv("DOMAIN") {
+		return false
+	}
+	return true
+}
--- go-url-shortner/api/routes/shorten.go
+++ go-url-shortner/api/routes/shorten.go
@@ -2,20 +2,48 @@ package routes
 
 import (
 	"time"
+
+	"github.com/gofiber/fiber"
 )
 
 type request struct {
-	URL		      	string			`json:"url"`
-	CustomShort		string			`json:"short"`
-	Expiry			time.Duration	`json:"expiry"`
+	URL         string        `json:"url"`
+	CustomShort string        `json:"short"`
+	Expiry      time.Duration `json:"expiry"`
 }
 
 type response struct {
-	URL					string			`json:"url"`
-	CustomShort		    string			`json:"short"`
-	Expiry				time.Duration   `json:"expiry"`
-	XRateRemaining		int				`json:"rate_limit"`
-	XRateLimitReset		time.Duration	`json:"rate_limit_reset"`
+	URL             string        `json:"url"`
+	CustomShort     string        `json:"short"`
+	Expiry          time.Duration `json:"expiry"`
+	XRateRemaining  int           `json:"rate_limit"`
+	XRateLimitReset time.Duration `json:"rate_limit_reset"`
 }
 
-func ShortenURL()
+func ShortenURL(c *fiber.Ctx) error {
+	body := new(request)
+	if err := c.BodyParser(&body); err != nil {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error":   true,
+			"message": "Cannot parse JSON",
+		})
+	}
+
+	//implement rate limiting
+	//check if input is an actual URL
+	if !govalidator.IsURL(body.URL) {
+		return c.Status(fiber.StatusBadRequest).JSON(fiber.Map{
+			"error":   true,
+			"message": "Invalid URL",
+		})
+	}
+	//check for domain error
+	if !helpers.RemoveDomainError(body.URL) {
+		return c.Status(fiber.StatusServiceUnavailable).JSON(fiber.Map{
+			"error":   true,
+			"message": "Domain not allowed",
+		})
+	}
+	//enforce http/ssl
+	body.URL = helpers.EnforceHTTP(body.URL)
+}
