f6b4299871b8703a6bb0f10f2ef82560412598df
shusin2798 <shubham.singh@syncron.com>
2025-04-02 11:14:45 +0530

    Created cutom short functionality and reponse

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # Golang
 Projects on GO.
+
+go-movie-crud := CRUD project on GO. Use structs as a datastore and acts as a database. 
+
+go-url-shorter := URL shortner made with go. Use redis to store the url and the corresponding short url.
--- go-url-shortner/api/routes/shorten.go
+++ go-url-shortner/api/routes/shorten.go
@@ -1,12 +1,16 @@
 package routes
 
 import (
-	"time"
-    "github.com/shusin2798/Golang/go-url-shortner/helpers"
-	"github.com/gofiber/fiber/v2"
-	"github.com/asaskevich/govalidator"
-	"github.com/shusin2798/Golang/go-url-shortner/database"
 	"os"
+	"time"
+
+	"strconv"
+
+	"github.com/asaskevich/govalidator"
+	"github.com/gofiber/fiber/v2"
+	"github.com/google/uuid"
+	"github.com/shusin2798/Golang/go-url-shortner/database"
+	"github.com/shusin2798/Golang/go-url-shortner/helpers"
 )
 
 type request struct {
@@ -39,15 +43,15 @@ func ShortenURL(c *fiber.Ctx) error {
 	if err == nil {
 		_ = r2.Set(database.Ctx, c.IP(), os.Getenv("API_QUOTA"), 30*30*time.Second).Err()
 	} else {
-		val, _ := r2.Get(database.Ctx, c.IP().Result())
 		valInt, _ := strconv.Atoi(val)
 		if valInt <= 0 {
 			limit, _ := r2.Get(database.Ctx, c.IP()).Result()
 			return c.Status(fiber.StatusTooManyRequests).JSON(fiber.Map{
-				"error":   true,
-				"message": "Rate limit exceeded",
+				"error":            true,
+				"message":          "Rate limit exceeded",
 				"rate_limit_reset": limit / time.Nanosecond / time.Minute,
 			})
+		}
 	}
 	//check if input is an actual URL
 	if !govalidator.IsURL(body.URL) {
@@ -66,5 +70,57 @@ func ShortenURL(c *fiber.Ctx) error {
 	//enforce http/ssl
 	body.URL = helpers.EnforceHTTP(body.URL)
 
+	var id string
+
+	if body.CustomShort == "" {
+		id = uuid.New().String()[:6]
+	} else {
+		id = body.CustomShort
+	}
+
+	r := database.CreateClient(0)
+	defer r.Close()
+
+	//check if custom short URL already exists
+	val, _ = r.Get(database.Ctx, id).Result()
+	if val != "" {
+		return c.Status(fiber.StatusForbidden).JSON(fiber.Map{
+			"error":   true,
+			"message": "Custom short URL already exists",
+		})
+	}
+	// set expiry time
+	if body.Expiry == 0 {
+		body.Expiry = 24
+	}
+
+	err = r.Set(database.Ctx, id, body.URL, body.Expiry*3600*time.Second).Err()
+
+	if err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+			"error":   true,
+			"message": "Unable to connect to server",
+		})
+	}
+
+	resp := response{
+		URL:             body.URL,
+		CustomShort:     "",
+		Expiry:          body.Expiry,
+		XRateRemaining:  10,
+		XRateLimitReset: 30,
+	}
+
 	r2.Decr(database.Ctx, c.IP())
+	val, _ = r2.Get(database.Ctx, c.IP()).Result()
+	resp.XRateRemaining, _ = strconv.Atoi(val)
+
+	var ttl time.Duration
+	ttl, _ = r2.TTL(database.Ctx, c.IP()).Result()
+	resp.XRateLimitReset = ttl / time.Nanosecond / time.Minute
+
+	resp.CustomShort = os.Getenv("BASE_URL") + "/" + id
+
+	return c.Status(fiber.StatusOK).JSON(resp)
+
 }
